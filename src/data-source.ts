493b028ee33d814d904bc967ae20854e9e2e7538
israel <debanjo31@gmail.com>
2025-10-04 13:16:19 +0100

    chore: update package.json to include tsx and mysql2, add dev script; modify data source for MySQL connection; add .env.example file

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,12 +1,20 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
 
 export const AppDataSource = new DataSource({
-    
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "my-secret-pw",
+  database: "test",
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+  extra: {
+    authPlugin: "mysql_native_password",
+  },
+});
